c9267281da9f5cd570956b7a2cb0015ce0338d90
KSXGitHub <hvksmr1996@gmail.com>
2015-12-26 19:24:49 +0700

    update

--- class/cplusplus-revison/codes/question-29.cpp
+++ class/cplusplus-revison/codes/question-29.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define DO_DAI 32;
+#define DO_DAI 32
 
 typedef int mang[DO_DAI];
 typedef int mang_tron[2 * DO_DAI];
@@ -21,4 +21,22 @@ int main() {
 	nhap(a, al);
 	cout << "Nhap do dai va cac phan tu cua mang b: ";
 	nhap(b, bl);
+	cout << "Mang a da nhap la: ";
+	xuat(a, al);
+	cout << "Mang b da nhap la: ";
+	xuat(b, bl);
+}
+
+void nhap(mang a, unsigned &l) {
+	cin >> l;
+	for (unsigned i = 0; i != l; ++i) {
+		cin >> a[i];
+	}
+}
+
+void xuat(mang a, unsigned l) {
+	for (int *i = a, *e = i + l; i != e; ++i) {
+		cout << '\x20' << *i;
+	}
+	cout << endl;
 }
