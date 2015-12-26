bf4e946fd81aa2657e1b9174e5e1d2e120759e6a
KSXGitHub <hvksmr1996@gmail.com>
2015-12-26 18:44:50 +0700

    a few first lines

--- class/cplusplus-revison/codes/question-30.cpp
+++ class/cplusplus-revison/codes/question-30.cpp
@@ -13,5 +13,16 @@ unsigned dem(chuoi);
 int kiem_tra(chuoi, chuoi);
 
 int main() {
-	
+	chuoi s1, s2;
+	nhap(s1, s2);
+	unsigned s1l = dem(s1), s2l = dem(s2);
+	cout << "Do dai chuoi s1: " << s1l << endl;
+	cout << "Do dai chuoi s2: " << s2l << endl;
+	int ket_qua_kiem_tra = s1l < s2l ? kiem_tra(s1, s2) : kiem_tra(s2, s1);
+	if (ket_qua_kiem_tra) {
+		cout << "Chuoi nho nam trong chuoi lon\n";
+	} else {
+		cout << "Chuoi nho khong nam trong chuoi lon\n";
+	}
+	return 0;
 }
