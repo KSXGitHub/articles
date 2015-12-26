91694f47f1e08599c3e720be60aef05dca00c056
KSXGitHub <hvksmr1996@gmail.com>
2015-12-26 19:33:42 +0700

    update

--- class/cplusplus-revison/codes/question-29.cpp
+++ class/cplusplus-revison/codes/question-29.cpp
@@ -10,6 +10,7 @@ typedef int mang_tron[2 * DO_DAI];
 
 int main();
 void nhap(mang, unsigned &);
+int kiem_tra(mang, unsigned);
 void xuat(mang, unsigned);
 void tron(mang, mang, mang_tron);
 
@@ -17,9 +18,9 @@ int main() {
 	mang a, b;
 	mang_tron c;
 	unsigned al, bl;
-	cout << "Nhap do dai va cac phan tu cua mang a: ";
+	cout << "Nhap do dai va cac phan tu cua mang a tang dan: ";
 	nhap(a, al);
-	cout << "Nhap do dai va cac phan tu cua mang b: ";
+	cout << "Nhap do dai va cac phan tu cua mang b tang dan: ";
 	nhap(b, bl);
 	cout << "Mang a da nhap la: ";
 	xuat(a, al);
@@ -28,10 +29,27 @@ int main() {
 }
 
 void nhap(mang a, unsigned &l) {
+	begin:
 	cin >> l;
 	for (unsigned i = 0; i != l; ++i) {
 		cin >> a[i];
 	}
+	if (!kiem_tra(a, l)) {
+		cout << "Mang da nhap khong hop le. Nhap lai: ";
+		goto begin;
+	}
+}
+
+int kiem_tra(mang a, unsigned l) {
+	int moc = a[0];
+	for (unsigned i = 1; i < l; ++i) {
+		if (a[i] > moc) {
+			moc = a[i];
+		} else {
+			return 0;
+		}
+	}
+	return 1;
 }
 
 void xuat(mang a, unsigned l) {
