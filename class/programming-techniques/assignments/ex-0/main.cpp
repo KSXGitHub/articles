107b3f7560d3505ace153648c4f8e12648c82aeb
KSXGitHub <hvksmr1996@gmail.com>
2016-01-14 21:44:54 +0700

    complete ex-0

---
+++ class/programming-techniques/assignments/ex-0/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main();
+double tong(unsigned, double);
+
+int main() {
+	unsigned n;
+	double x;
+	cout << "Nhap lan luot n va x: ";
+	cin >> n >> x;
+	cout << "S("<< n << ", " << x << ") = " << tong(n, x) << endl;
+	return 0;
+}
+
+double tong(unsigned n, double x) {
+	double S = 0, tich = 1;
+	unsigned giaithua = 1;
+	for (unsigned i = 1; i <= n; ++i) {
+		tich *= x;
+		giaithua *= i;
+		S += tich / giaithua;
+	}
+	return S;
+}
