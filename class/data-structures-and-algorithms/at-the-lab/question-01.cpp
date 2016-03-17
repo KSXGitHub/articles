9571e3201fb9d959c2e358afded2722ac02c6434
Khải <hvksmr1996@gmail.com>
2016-03-17 09:31:08 +0700

    Update question-01.cpp

--- class/data-structures-and-algorithms/at-the-lab/question-01.cpp
+++ class/data-structures-and-algorithms/at-the-lab/question-01.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+
+using namespace std;
+
+const int MAX_LENGTH = 20;
+
+struct Mang {
+	int phantu[MAX_LENGTH];
+	unsigned dodai;
+};
+
+int main();
+void khoitao(Mang &);
+void nhap(Mang &);
+void xuat(Mang &);
