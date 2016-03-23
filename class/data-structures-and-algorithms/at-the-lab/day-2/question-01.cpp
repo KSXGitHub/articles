551ba1a1165389045eab839cda9d41fdba6dc080
Khải <hvksmr1996@gmail.com>
2016-03-23 08:30:13 +0700

    Update question-01.cpp

--- class/data-structures-and-algorithms/at-the-lab/day-2/question-01.cpp
+++ class/data-structures-and-algorithms/at-the-lab/day-2/question-01.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main();
+unsigned UCLN(unsigned, unsigned);
+
+int main() {
+	unsigned a, b, c;
+	cout << "Nhap 3 so nguyen: ";
+	cin >> a >> b >> c;
+	cout << "UCLN(a, b, c) = " << UCLN(UCLN(a, b), c) << endl;
+	return 0;
+}
+
+unsigned UCLN(unsigned a, unsigned b) {
+	if (a == b) {
+		return a;
+	}
+	return a > b ? UCLN(a - b, b) : UCLN(b - a, a);
+}
