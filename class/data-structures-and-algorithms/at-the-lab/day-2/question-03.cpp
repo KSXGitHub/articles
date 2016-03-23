0dd785ba7ba5920b4d148838bd84292533290b25
Khải <hvksmr1996@gmail.com>
2016-03-23 09:10:47 +0700

    Update question-03.cpp

--- class/data-structures-and-algorithms/at-the-lab/day-2/question-03.cpp
+++ class/data-structures-and-algorithms/at-the-lab/day-2/question-03.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+constexpr BITLENGTH = sizeof(unsigned) * 8;
+typedef char BinStr[BITLENGTH];
+
+int main();
+unsigned fromBinStr(BinStr);
+
+int main() {
+	BinStr bs;
+	cout << "Nhap mot chuoi nhi phan: ";
+	cin.getline(bs, BITLENGTH);
+	cout << "Dang thap phan: " << fromBinStr(bs) << endl;
+	return 0;
+}
+
+unsigned fromBinStr(BinStr bs) {
+	if (*bs) {
+		return ((*bs - '0') << 1) + fromBinStr(bs + 1);
+	}
+	return 0;
+}
