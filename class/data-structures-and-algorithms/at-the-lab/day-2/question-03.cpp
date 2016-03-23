963eb5d7a8cc8f85794751ded65ab7c710881f8f
Khải <hvksmr1996@gmail.com>
2016-03-23 09:28:30 +0700

    Update question-03.cpp

--- class/data-structures-and-algorithms/at-the-lab/day-2/question-03.cpp
+++ class/data-structures-and-algorithms/at-the-lab/day-2/question-03.cpp
@@ -15,13 +15,13 @@ int main() {
 	cout << "Nhap mot chuoi nhi phan: ";
 	cin.getline(bs, BITLENGTH);
 	unsigned length = strlen(bs);
-	cout << "Dang thap phan: " << fromBinStr(bs + length - 1, bs + length) << endl;
+	cout << "Dang thap phan: " << fromBinStr(bs + length - 1, length) << endl;
 	return 0;
 }
 
 unsigned fromBinStr(char *end, unsigned length) {
 	if (length) {
-		return ((*end - '0') << length) + fromBinStr(end - 1, length - 1);
+		return (*end - '0') + (fromBinStr(end - 1, length - 1) << 1);
 	}
 	return 0;
 }
