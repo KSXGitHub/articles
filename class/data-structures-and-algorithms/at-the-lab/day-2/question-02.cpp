b55671c44728509ecfba91d0b1e15b9e28563199
Khải <hvksmr1996@gmail.com>
2016-03-23 08:45:15 +0700

    Update question-02.cpp

--- class/data-structures-and-algorithms/at-the-lab/day-2/question-02.cpp
+++ class/data-structures-and-algorithms/at-the-lab/day-2/question-02.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main();
+unsigned sum(unsigned);
+
+int main() {
+	unsigned n;
+	cout << "n = ";
+	cin >> b;
+	cout << "S(n) = " << sum(n) << endl;
+	return 0;
+}
+
+unsigned sum(unsigned n) {
+	return n && (n + sum(n - 1));
+}
