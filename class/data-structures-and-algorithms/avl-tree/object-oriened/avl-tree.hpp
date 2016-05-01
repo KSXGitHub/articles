aa3c17b0df50e9696072e5c8ff71899c36fee003
khai96_ <hvksmr1996@gmail.com>
2016-05-01 07:46:42 +0700

    update header file

---
+++ class/data-structures-and-algorithms/avl-tree/object-oriened/avl-tree.hpp
@@ -0,0 +1,200 @@
+
+#include <cstdlib>
+
+template <class Data>
+class AVLTree {
+
+private:
+
+    /* TYPES AND CLASSES */
+
+    enum Balance {
+        LEFT, NOSIDE, RIGHT
+    };
+
+    struct Node {
+
+        Data data;
+        Node *left, *right;
+        Balance balance;
+
+        Node(Data data = Data()) {
+            Node::data = data;
+            left = right = NULL;
+            balance = Balance::NOSIDE;
+        }
+
+        ~Node() {
+            delete left;
+            delete right;
+        }
+
+    };
+
+    /* FUNCTIONS */
+
+    void _addNode(Node *&origin, Data data, bool &rotate) {
+        if (!origin) {
+            origin = new Node(data);
+            rotate = true;
+        } else if (data < origin->data) {
+            _addNode(origin->left, data, rotate);
+            if (rotate) switch (origin->balance) {
+                case Balance::LEFT:
+                    _rotateLeft(origin);
+                    origin->balance = Balance::NOSIDE;
+                    rotate = false;
+                    break;
+                case Balance::NOSIDE:
+                    origin->balance = Balance::LEFT;
+                    break;
+                case Balance::RIGHT:
+                    origin->balance = Balance::NOSIDE;
+                    rotate = false;
+                    break;
+            }
+        } else if (data > origin->data) {
+            _addNode(origin->right, data, rotate);
+            if (rotate) switch (origin->balance) {
+                case Balance::LEFT:
+                    origin->balance = Balance::NOSIDE;
+                    rotate = false;
+                    break;
+                case Balance::NOSIDE:
+                    origin->balance = Balance::RIGHT;
+                    break;
+                case Balance::RIGHT:
+                    _rotateRight(origin);
+                    origin->balance = Balance::NOSIDE;
+                    rotate = false;
+            }
+        } else {
+            rotate = false;
+        }
+    }
+
+    void _rotateLeft(Node *&origin) {
+        Node *p1 = origin->left;
+        if (p1->balance == Balance::LEFT) {
+            origin->left = p1->right;
+            p1->right = origin;
+            origin = p1;
+        } else {
+            Node *p2 = p1->right;
+            origin->left = p2->right;
+            p1->right = p2->left;
+            p2->left = p1;
+            p2->right = origin;
+            switch (p2->balance) {
+                case Balance::LEFT:
+                    p1->balance = Balance::RIGHT;
+                    origin->balance = Balance::NOSIDE;
+                    break;
+                case Balance::RIGHT:
+                    p1->balance = Balance::NOSIDE;
+                    origin->balance = Balance::LEFT;
+                    break;
+                default:
+                    p1->balance = origin->balance = Balance::NOSIDE;
+            }
+            origin = p2;
+        }
+    }
+
+    void _rotateRight(Node *&origin) {
+        Node *p1 = origin->right;
+        if (p1->balance == Balance::RIGHT) {
+            origin->right = p1->left;
+            p1->left = origin;
+            origin = p1;
+        } else {
+            Node *p2 = p1->left;
+            origin->right = p2->left;
+            p1->left = p2->right;
+            p2->right = p1;
+            p2->left = origin;
+            switch (p2->balance) {
+                case Balance::RIGHT:
+                    p1->balance = Balance::LEFT;
+                    origin->balance = Balance::NOSIDE;
+                    break;
+                case Balance::LEFT:
+                    p1->balance = Balance::NOSIDE;
+                    origin->balance = Balance::RIGHT;
+                    break;
+                default:
+                    p1->balance = origin->balance = Balance::NOSIDE;
+            }
+            origin = p2;
+        }
+    }
+
+    /* DATA */
+
+    Node *origin;
+
+public:
+
+    /* TYPES AND CLASSES */
+
+    struct TRAVERSE {
+        struct Order {};
+        static struct PreOrder : Order {} PRE_ORDER;
+        static struct MidOrder : Order {} MID_ORDER;
+        static struct PostOrder : Order {} POST_ORDER;
+    };
+
+    /* CONSTRUCTORS */
+
+    AVLTree(Node origin = NULL) {
+        AVLTree::origin = origin;
+    }
+
+    ~AVLTree() {
+        delete origin;
+    }
+
+    /* FUNCTIONS */
+
+    auto data() {
+        return origin->data;
+    }
+
+    auto data(Data value) {
+        origin->data = value;
+        return this;
+    }
+
+    auto left() {
+        return AVLTree(origin->left);
+    }
+
+    auto left(AVLTree tree) {
+        origin->left = tree.origin;
+    }
+
+    auto right() {
+        return AVLTree(origin->right);
+    }
+
+    auto right(AVLTree tree) {
+        origin->right = tree.origin;
+    }
+
+    void addNode(Data data) {
+        bool rotate = false;
+        _addNode(origin, data, rotate);
+    }
+
+    void traverse(TRAVERSE::PreOrder order, void (*onnode)(Data *, unsigned), void (*onnull)(unsigned), unsigned level = 0) {
+        if (origin) {
+            onnode(&origin->data, level);
+            ++level;
+            left().traverse(order, onnode, onnull);
+            right().traverse(order, onnode, onnull);
+        } else {
+            onnull(level);
+        }
+    }
+
+};
