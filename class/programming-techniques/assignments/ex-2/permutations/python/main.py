a3c1949fd292efb2713b56cb5cabe6594acbdc27
khai96_ <hvksmr1996@gmail.com>
2016-05-02 14:44:06 +0700

    complete python solution

--- class/programming-techniques/assignments/ex-2/permutations/python/main.py
+++ class/programming-techniques/assignments/ex-2/permutations/python/main.py
@@ -6,10 +6,10 @@ count = int(input("Nhap so luong cac phan tu cua moi hoan vi: "));
 if count < 0:
     raise "So luong phan tu moi hoan vi phai la mot so tu nhien"
 
-pattern = [];
+pattern = set();
 i = 0;
 while i != count:
-    pattern += [i]
+    pattern.add(i)
     i += 1
     pass
 
--- class/programming-techniques/assignments/ex-2/permutations/python/permutations.py
+++ class/programming-techniques/assignments/ex-2/permutations/python/permutations.py
@@ -1,1 +1,11 @@
 
+def permutations(pattern):
+
+    if (pattern.__len__()):
+        result = set();
+        for i in result:
+            for j in permutations(pattern - {i}):
+                result.add([i] + j)
+
+    else:
+        return {[]}
