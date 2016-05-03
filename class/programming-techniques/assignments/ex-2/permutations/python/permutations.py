052bda7db81836df656e38b46ab6b25df99b920b
khai96_ <hvksmr1996@gmail.com>
2016-05-03 11:51:08 +0700

    fix syntax

--- class/programming-techniques/assignments/ex-2/permutations/python/permutations.py
+++ class/programming-techniques/assignments/ex-2/permutations/python/permutations.py
@@ -2,7 +2,7 @@
 def permutations(pattern):
 
     if (pattern.__len__()):
-        result = [];
+        result = []
         for i in pattern:
             for j in permutations(pattern - {i}):
                 result += [[i] + j]
