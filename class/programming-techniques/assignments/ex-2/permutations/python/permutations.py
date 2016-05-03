422fcc2aaa88132922e2a4824252766e26752c24
khai96_ <hvksmr1996@gmail.com>
2016-05-03 12:25:45 +0700

    use 'len' instead of '__len__'

--- class/programming-techniques/assignments/ex-2/permutations/python/permutations.py
+++ class/programming-techniques/assignments/ex-2/permutations/python/permutations.py
@@ -1,7 +1,7 @@
 
 def permutations(pattern):
 
-    if (pattern.__len__()):
+    if (len(pattern)):
         result = []
         for i in pattern:
             for j in permutations(pattern - {i}):
